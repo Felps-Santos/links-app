99939968b9925ac83a6febcd2e671384f7c2bbe3
Felipe Dos Santos Silva <felipessilva179@gmail.com>
2024-12-29 15:44:19 -0300

    App Finalizado

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,83 +1,134 @@
-import { View, Image, TouchableOpacity, FlatList, Modal, Text, Alert } from 'react-native';
-import { useState, useCallback } from 'react';
-import { router, useFocusEffect } from 'expo-router';
-import { s } from './styles';
-import { MaterialIcons } from '@expo/vector-icons';
-import { colors } from '@/styles/colors';
-import { Categories } from '@/components/categories';
-import { Link } from '@/components/link';
-import { Option } from '@/components/option';
-import { categories } from '@/utils/categories';
-import { LinkStorage, linkStorage } from '@/storage/link-storage';
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+  Alert,
+  Linking,
+} from "react-native";
+import { useState, useCallback } from "react";
+import { router, useFocusEffect } from "expo-router";
+import { s } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
+import { Categories } from "@/components/categories";
+import { Link } from "@/components/link";
+import { Option } from "@/components/option";
+import { categories } from "@/utils/categories";
+import { LinkStorage, linkStorage } from "@/storage/link-storage";
 
 export default function Index() {
-    const [category, setCategory] = useState(categories[0].name)
-    const [links, setLinks] = useState<LinkStorage[]>([])
+  const [showModal, setShowModal] = useState(false);
+  const [category, setCategory] = useState(categories[0].name);
+  const [links, setLinks] = useState<LinkStorage[]>([]);
+  const [link, setLink] = useState<LinkStorage>({} as LinkStorage);
 
-    async function getLinks() {
-        try {
-            const response = await linkStorage.get()
-            setLinks(response)
-        } catch(error) {
-            Alert.alert('Erro', 'Não foi possível listar os links')
-        }
+  async function getLinks() {
+    try {
+      const response = await linkStorage.get();
+      const filtered = response.filter((link) => link.category === category);
+      setLinks(filtered);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível listar os links");
     }
+  }
 
-    useFocusEffect(useCallback(() => {
-        getLinks()
-    }, []))
+  function handleDetails(selected: LinkStorage) {
+    setShowModal(true);
+    setLink(selected);
+  }
 
-    return (
-        <View style={s.container}>
-            <View style={s.header}>
-                <Image source={require('@/assets/logo.png')} style={s.logo}/>
-                <TouchableOpacity onPress={() => router.navigate('/add')}>
-                    <MaterialIcons name='add' size={32} color={colors.green[300]}/>
-                </TouchableOpacity>
+  async function linkRemove() {
+    try {
+      await linkStorage.remove(link.id);
+      getLinks();
+      setShowModal(false);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível excluir");
+      console.log(error);
+    }
+  }
+
+  function handleRemove() {
+    Alert.alert("Excluir", "Deseja realmente excluir ?", [
+      {
+        style: "cancel",
+        text: "Não",
+      },
+      {
+        text: "Sim",
+        onPress: () => linkRemove,
+      },
+    ]);
+  }
+
+  async function handleOpen() {
+    try {
+      await Linking.openURL(link.url);
+    } catch (error) {
+      Alert.alert("Link", "Não foi possível abrir o link");
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      getLinks();
+    }, [category])
+  );
+
+  return (
+    <View style={s.container}>
+      <View style={s.header}>
+        <Image source={require("@/assets/logo.png")} style={s.logo} />
+        <TouchableOpacity onPress={() => router.navigate("/add")}>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+      <Categories onChange={setCategory} selected={category} />
+      <FlatList
+        data={links}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Link
+            name={item.name}
+            url={item.url}
+            onDetails={() => handleDetails(item)}
+          />
+        )}
+        style={s.links}
+        contentContainerStyle={s.linksContent}
+        showsVerticalScrollIndicator={false}
+      />
+      <Modal transparent visible={showModal} animationType="slide">
+        <View style={s.modal}>
+          <View style={s.modalContent}>
+            <View style={s.modalHeader}>
+              <Text style={s.modalCategory}>{link.category}</Text>
+              <TouchableOpacity onPress={() => setShowModal(false)}>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
             </View>
-            <Categories onChange={setCategory} selected={category}/>
-            <FlatList 
-                data={links}
-                keyExtractor={(item) => item.id}
-                renderItem={( {item }) => (
-                    <Link 
-                        name={item.name} 
-                        url={item.url} 
-                        onDetails={() => console.log("Clicou")}
-                    />
-                )}
-                style={s.links}
-                contentContainerStyle={s.linksContent}
-                showsVerticalScrollIndicator={false}
-            />
-            <Modal transparent visible={false}>
-                <View style={s.modal}>
-                    <View style={s.modalContent}>
-                        <View style={s.modalHeader}>
-                            <Text style={s.modalCategory}>
-                                Curso
-                            </Text>
-                            <TouchableOpacity>
-                                <MaterialIcons 
-                                    name='close' 
-                                    size={20} 
-                                    color={colors.gray[400]}
-                                />
-                            </TouchableOpacity>
-                        </View>
-                        <Text style={s.modalLinkName}>
-                            Rocketseat
-                        </Text>
-                        <Text style={s.modalUrl}>
-                            https://www.rocketseat.com.br/
-                        </Text>
-                        <View style={s.modalFooter}>
-                            <Option name='Excluir' icon='delete' variant='secondary'/>
-                            <Option name='Abrir' icon='language'/>
-                        </View>
-                    </View>
-                </View>
-            </Modal>
+            <Text style={s.modalLinkName}>{link.name}</Text>
+            <Text style={s.modalUrl}>{link.url}</Text>
+            <View style={s.modalFooter}>
+              <Option
+                name="Excluir"
+                icon="delete"
+                variant="secondary"
+                onPress={handleRemove}
+              />
+              <Option name="Abrir" icon="language" onPress={handleOpen} />
+            </View>
+          </View>
         </View>
-    )
+      </Modal>
+    </View>
+  );
 }
--- src/storage/link-storage.ts
+++ src/storage/link-storage.ts
@@ -10,32 +10,42 @@ links: [{ name: "Rocket", url: "123" }, {}, {}]
 
 */
 
-import AsyncStorage from "@react-native-async-storage/async-storage"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const LINKS_STORAGE_KEY = "links-storage"
+const LINKS_STORAGE_KEY = "links-storage";
 
 export type LinkStorage = {
-    id: string
-    name: string
-    url: string
-    category: string
-}
+  id: string;
+  name: string;
+  url: string;
+  category: string;
+};
 
 async function get(): Promise<LinkStorage[]> {
-    const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY)
-    const response = storage ? JSON.parse(storage) : []
+  const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY);
+  const response = storage ? JSON.parse(storage) : [];
 
-    return response
+  return response;
 }
 
 async function save(newLink: LinkStorage) {
-    try{
-        const storage = await get()
-        const updated = JSON.stringify([...storage, newLink])
-        await AsyncStorage.setItem(LINKS_STORAGE_KEY, updated)
-    } catch(error) {
-        throw error
-    }
+  try {
+    const storage = await get();
+    const updated = JSON.stringify([...storage, newLink]);
+    await AsyncStorage.setItem(LINKS_STORAGE_KEY, updated);
+  } catch (error) {
+    throw error;
+  }
 }
 
-export const linkStorage = { get, save }
+async function remove(id: string) {
+  try {
+    const storage = await get();
+    const updated = storage.filter((link) => link.id !== id);
+    await AsyncStorage.setItem(LINKS_STORAGE_KEY, JSON.stringify(updated));
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const linkStorage = { get, save, remove };
